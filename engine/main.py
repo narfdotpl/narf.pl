efa0dd6ddbb5310ef3e210badf871267768cb085
Maciej Konieczny <hello@narf.pl>
2014-10-30 21:37:19 +0100

    added grammatic redirect

--- engine/main.py
+++ engine/main.py
@@ -382,6 +382,7 @@ SPECIFIC_REDIRECTS = {
     '/feed.xml': '/feed',
     '/plain.txt': '/posts/plain-text',
     '/quit.txt': '/posts/quit-delicious',
+    '/liczba-nie-ilosc': 'http://www.youtube.com/watch?v=Gc31UQ-C6dw',
     '/samo-sie-nie-zrobi': 'http://www.youtube.com/watch?v=xOUjIr70XgQ',
 }
 LAB_REDIRECT_PREFIXES = ['canvas-pong', 'jquery-typing', 'tmp']
