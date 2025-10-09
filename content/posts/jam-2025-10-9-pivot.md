57e4f5883159328a8982afa83198bd755c7392af
Maciej Konieczny <hello@narf.pl>
2025-10-09 16:15:50 +0200

    added Pivot and Nudistort jam

---
+++ content/posts/jam-2025-10-9-pivot.md
@@ -0,0 +1,11 @@
+date: 2025-10-09
+collections: [jam]
+theme: black
+
+Jam: Fors Pivot and Nudistort drone
+===================================
+
+{{ youtube_iframe('-0E0nF-7jtI') }}
+
+— what do you want to do when you grow up?<br/>
+— noise
--- engine/main.py
+++ engine/main.py
@@ -1082,9 +1082,6 @@ def unmatched_path_to_post_url(path):
     >>> unmatched_path_to_post_url('esc')
     'https://narf.pl/music/escapism'
 
-    >>> unmatched_path_to_post_url('a')
-    'https://narf.pl/posts/autoportrait-2025'
-
     >>> unmatched_path_to_post_url('e')
     'https://narf.pl/posts/leaving'
 
